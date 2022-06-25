af95a2f7869540a6dac04b768dfa846a95125f27
Shashank B <97756483+Shashankkk-123@users.noreply.github.com>
2022-06-25 07:59:24 +0530

    Update and rename PP_EXP_4_4.2.py to CreditCardNumberChecker.py

--- CreditCardNumberChecker.py
+++ CreditCardNumberChecker.py
@@ -5,7 +5,7 @@ num = input("enter your Credit Card Number:")
 print(len(num))
 
 if re.search(r'^[456]+', num)  and  re.search(r'[\d]', num)  and bool(re.search(r'\s', num))is False and bool (re.search(r'[a-z]', num)) is False and (bool(re.search(r'-', num)) is True and len(num) == 19):
-    print("The entered Credit Card Nmber is valid")
+    print("The entered Credit Card Number is valid")
 
 
 else:
